7317a2d476ec6bf8c79479fe5d8a57982bca073f
zarcven <zarc771048@gmail.com>
2025-02-19 23:45:14 +0800

    Answered question 15

--- Php Part 1/Answers for Questions/Question_15.php
+++ Php Part 1/Answers for Questions/Question_15.php
@@ -1,2 +1,23 @@
 <?php
+$fruits = ['apple','banana','orange'];
+$chunkedArray = array_chunk($fruits, 2);
+var_dump($chunkedArray);
+/*
+Question 15: What is the output of var_dump($chunkedArray);?
+Answer:
+array(2) {
+  [0]=>
+  array(2) {
+    [0]=>
+    string(5) "apple"
+    [1]=>
+    string(6) "banana"
+  }
+  [1]=>
+  array(1) {
+    [0]=>
+    string(6) "orange"
+  }
+}
+*/
 ?>
