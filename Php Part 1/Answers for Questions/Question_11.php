90d2ef775c72991be5f7d904f18fc8831a8c0a37
zarcven <zarc771048@gmail.com>
2025-02-19 23:11:53 +0800

    update question_11.php

---
+++ Php Part 1/Answers for Questions/Question_11.php
@@ -0,0 +1,12 @@
+<?php
+$hex = [
+    'red' => '#f00',
+    'green' => '#0f0',
+    'blue' => '#00f'
+];
+echo $hex['red'];
+/*
+Question 11: What is the output of echo $hex['red'];?
+Answer: #f00
+*/
+?>
